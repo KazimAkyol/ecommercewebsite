51b027401216582e9ef565c9d1763d987f43ef2a
KazimAkyol <akyolkzm.2016493@gmail.com>
2025-12-14 20:40:41 +0100

    basic structure setup

---
+++ app/components/home/Category.tsx
@@ -0,0 +1,30 @@
+"use client"
+
+const Category = () => {
+    const categoryList = [
+        {
+            name: "Ayakkabi"
+        },
+        {
+            name: "Ayakkabi"
+        },
+        {
+            name: "Ayakkabi"
+        },
+        {
+            name: "Ayakkabi"
+        },
+        {
+            name: "Ayakkabi"
+        },
+        {
+            name: "Ayakkabi"
+        },
+    ]
+
+    return (
+        <div>Category</div>
+    )
+}
+
+export default Category
