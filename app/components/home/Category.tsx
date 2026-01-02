c0448dd221e6d80249a43f70fa5bd98570a27f74
KazimAkyol <akyolkzm.2016493@gmail.com>
2026-01-02 18:05:29 +0100

    updated Category page

--- app/components/home/Category.tsx
+++ app/components/home/Category.tsx
@@ -20,11 +20,20 @@ const Category = () => {
         {
             name: "Ayakkabi"
         },
+        {
+            name: "Ayakkabi"
+        },
     ]
 
     return (
         <div className="flex items-center justify-center px:3 md:px-10 gap-3 md:gap-10 py-5 md:py-8 overflow-x-auto">
-            Category
+            {
+                categoryList.map((category, index) => (
+                    <div className="border text-slate-500 rounded-full min-w-30 flex items-center justify-center cursor-pointer flex-1 px-2 py-2 text-center" key={index}>
+                        {category.name}
+                    </div>
+                ))
+            }
         </div>
     )
 }
