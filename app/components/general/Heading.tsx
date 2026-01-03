3b4309ea0fa20f15edd2440527f71c6ebffa9c69
KazimAkyol <akyolkzm.2016493@gmail.com>
2026-01-03 23:54:00 +0100

    Products page definition

--- app/components/general/Heading.tsx
+++ app/components/general/Heading.tsx
@@ -1,12 +1,15 @@
-
+/* Heading bilesenine hangi props'larin (özelliklerin) gönderilecegini tanimlayan bir arayüz (interface) */
 interface HeadingProps {
     center?: boolean;
     text: string
 }
 
+/* React.FC<HeadingProps>: Bu, Heading bileseninin bir functional component oldugunu ve props'larin HeadingProps arayüzüne uygun olmasi gerektigini belirtir */
 const Heading: React.FC<HeadingProps> = ({ center, text }) => {
     return (
-        <div className={`text-slate-500 my-3 md:my-5 px-3 md:px-10 md:text-xl ${center ? "text-center" : "text-start"}`}>{text}</div>
+        <div className={`text-slate-500 my-3 md:my-5 px-3 md:px-10 md:text-xl ${center ? "text-center" : "text-start"}`}>
+            {text}
+        </div>
     )
 }
 
---
+++ app/components/home/Products.tsx
@@ -0,0 +1,11 @@
+import Heading from "../general/Heading"
+
+const Products = () => {
+    return (
+        <div>
+            <Heading text="Tüm ürünler" />
+        </div>
+    )
+}
+
+export default Products
