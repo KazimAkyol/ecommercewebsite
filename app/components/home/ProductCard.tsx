742d8b0b0d91b41ee565e846e4c1896f20dc71b3
KazimAkyol <akyolkzm.2016493@gmail.com>
2026-01-05 23:12:37 +0100

    updated-2 ProductCard page

--- app/components/home/ProductCard.tsx
+++ app/components/home/ProductCard.tsx
@@ -3,12 +3,15 @@
 import textClip from "@/utils/TextClip"
 import { Rating } from "@mui/material"
 import Image from "next/image"
+import { useRouter } from "next/navigation"
 
 const ProductCard = ({ product }: { product: any }) => {
+    const router = useRouter();
 
     let productRating = product?.reviews?.reduce((acc: number, item: any) => acc + item.rating, 0) / product?.reviews?.length
     return (
-        <div className="w-60 cursor-pointer flex flex-col flex-1 shadow-lg p-2 rounded-md">
+        <div onClick={() => router.push(`product/${product.id}`)}
+            className="w-60 cursor-pointer flex flex-col flex-1 shadow-lg p-2 rounded-md">
             <div className="relative h-37.5">
                 <Image src={product.image} fill alt="" className="object-contain" />
             </div>
--- app/product/[productId]/page.tsx
+++ app/product/[productId]/page.tsx
@@ -1,7 +1,9 @@
+import React from 'react'
 import DetailClient from "@/app/components/detail/DetailClient";
+import { products } from '@/utils/Products';
 
 type DetailProps = {
-    productId: string
+    productId?: string
 }
 
 const Detail = ({ params }: { params: DetailProps }) => {
@@ -9,6 +11,8 @@ const Detail = ({ params }: { params: DetailProps }) => {
     const { productId } = params;
 
     const product = products.find(product => product.id == productId);
+
+    console.log(product, "product");
 
     return (
         <div>
