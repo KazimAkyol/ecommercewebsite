ff41013632e500fdaf31f8299a44e34d299d0beb
KazimAkyol <akyolkzm.2016493@gmail.com>
2026-01-06 23:29:35 +0100

    styled DetailClient page

--- app/components/detail/DetailClient.tsx
+++ app/components/detail/DetailClient.tsx
@@ -7,11 +7,20 @@ const DetailClient = ({ product }: { product: any }) => {
     return (
         <div className="my-10">
             <PageContainer>
-                <div className="block md:flex">
+                <div className="block md:flex gap-10 justify-center">
                     <div className="relative h-100 w-50 flex-1">
                         <Image src={product?.image} fill alt="" />
                     </div>
-                    <div className="w-3/4">right</div>
+                    <div className="w-1/2 space-y-3">
+                        <div className="text-xl md:text-2xl">{product?.name}</div>
+                        <div className="text-slate-500">{product?.description}</div>
+                        <div>
+                            <div>STOK DURUMU:</div>
+                            {
+                                product.inStock ? <div className="text-green-500">Ürün Stokta Mevcut</div> : <div className="text-red-500">Ürün Stokta Bulunmamakta</div>
+                            }
+                        </div>
+                    </div>
                 </div>
             </PageContainer>
         </div>
