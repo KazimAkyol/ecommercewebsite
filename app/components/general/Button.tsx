f6a80652321e4943f87d4fd48be3f75050a90413
KazimAkyol <akyolkzm.2016493@gmail.com>
2026-01-07 17:00:48 +0100

    updated Button page

--- app/components/general/Button.tsx
+++ app/components/general/Button.tsx
@@ -1,12 +1,25 @@
+import { IconType } from "react-icons";
 
 interface ButtonProps {
     text: string;
     onClick: (e: React.MouseEvent<HTMLButtonElement>) => void;
+    small?: boolean;
+    outline?: boolean;
+    icon?: IconType | undefined;
+    disabled?: boolean;
+    className?: string;
 }
 
-const Button = () => {
+const Button: React.FC<ButtonProps> = ({ text, onClick, small, outline, disabled, icon: Icon, className }: ButtonProps) => {
     return (
-        <button onClick={() => { }}>Button</button>
+        <button
+            disabled={disabled}
+            className={`my-1 flex items-center justify-center gap-2 rounded-lg p-3 ${small ? "w-62.5" : "w-full"} ${outline ? "border text-black" : "bg-black text-white"} ${className}`}
+            onClick={onClick}
+        >
+            {icon && <Icon />}
+            {text}
+        </button>
     )
 }
 
