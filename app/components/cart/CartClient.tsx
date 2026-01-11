c3bc6d225d3f2a6a9ae95e841f5abb8b73c8aa7b
KazimAkyol <akyolkzm.2016493@gmail.com>
2026-01-11 16:21:09 +0100

    styled CartClient.tsx

--- app/components/cart/CartClient.tsx
+++ app/components/cart/CartClient.tsx
@@ -2,6 +2,7 @@
 
 import UseCart from "@/hooks/useCart"
 import PageContainer from "../containers/PageContainer"
+import Image from "next/image";
 
 const CartClient = () => {
 
@@ -9,14 +10,35 @@ const CartClient = () => {
 
     console.log(cartPrdcts, "cartPrdcts");
 
-    return (
-        <div>
-            <PageContainer>
-                <div>
+    if (!cartPrdcts || cartPrdcts.length == 0)
+        return <div>Sepetinizde ürün bulunmamaktadir...</div>
 
+    return (
+        <div className="my-3 md:my-10">
+            <PageContainer>
+                <div className="flex items-center gap-3 text-center border-b py-3 font-semibold">
+                    <div className="w-1/5">Ürün Resmi</div>
+                    <div className="w-1/5">Ürün Adi</div>
+                    <div className="w-1/5">Ürün Miktari</div>
+                    <div className="w-1/5">Ürün Fiyati</div>
+                    <div className="w-1/5"></div>
+                </div>
+                <div>
+                    {
+                        cartPrdcts.map(cart => (
+                            <div className="flex items-center justify-between text-center my-5" key={cart.id}>
+                                <div>
+                                    <Image src={cart.image} width={100} height={100} alt="" />
+                                </div>
+                                <div>{cart.name}</div>
+                                <div>{cart.quantity}</div>
+                                <div>{cart.price} $</div>
+                                <div>Ürün </div>
+                            </div>
+                        ))
+                    }
                 </div>
             </PageContainer>
-
         </div>
     )
 }
