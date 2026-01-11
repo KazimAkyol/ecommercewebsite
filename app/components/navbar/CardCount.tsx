d1ccb12558201fabe3b4b54af9d3c80994e3aaba
KazimAkyol <akyolkzm.2016493@gmail.com>
2026-01-11 17:41:05 +0100

    added hot-toast

--- app/components/navbar/CardCount.tsx
+++ app/components/navbar/CardCount.tsx
@@ -1,11 +1,19 @@
 "use client"
 
+import UseCart from "@/hooks/useCart";
 import { MdShoppingBasket } from "react-icons/md";
 
 const CardCount = () => {
+
+    const { cartPrdcts } = UseCart();
+
     return (
-        <div className="hidden md:flex">
-            <MdShoppingBasket size="25" />
+        <div className="hidden md:flex relative">
+            <MdShoppingBasket size="30" />
+            <div className="absolute -top-1 -right-2 text-xs bg-orange-900 w-5 h-5 flex items-center justify-center rounded-full">
+                {/* -top-1: yukarida 1 kismi, -right-2: saga 2 kismi */}
+                {cartPrdcts?.length}
+            </div>
         </div>
     )
 }
