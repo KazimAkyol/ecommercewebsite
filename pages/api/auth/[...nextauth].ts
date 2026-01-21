e1013aba0ef896fc99503c1b18d1930faef0911f
KazimAkyol <akyolkzm.2016493@gmail.com>
2026-01-21 15:54:22 +0100

    added @next-auth/prisma-adapter structure

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,8 +1,7 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
-import { PrismaAdapter } from "@auth/prisma-adapter";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { PrismaClient } from "@prisma/client";
-import Google from "next-auth/providers/google";
 
 const prisma = new PrismaClient()
 
@@ -11,7 +10,7 @@ export default NextAuth({
     providers: [
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID as string,
-            
-        })
-    ]
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+        }),
+    ],
 })
