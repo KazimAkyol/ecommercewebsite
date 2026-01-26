15a8c7df93c436ecfa1c55ebacf150ef4716ec2e
KazimAkyol <akyolkzm.2016493@gmail.com>
2026-01-26 21:55:54 +0100

    added bcrypt structure

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -22,7 +22,17 @@ export default NextAuth({
                 if (!credentials?.email || !credentials?.password) {
                     throw new Error("Invalid email or password")
                 }
+                const user = await prisma.user.findUnique({
+                    where: {
+                        email: credentials.email
+                    }
+                })
 
+                if (!user || !user.hashedPassword) {
+                    throw new Error("Invalid email or password")
+                }
+
+                const comparePassword =
             }
         })
     ],
